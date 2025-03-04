23694d1d68ca494bc90dd1fcd86d5c0d72ff5d80
Noppanat Wadlom <noppanat.wad@gmail.com>
2025-03-05 04:26:33 +0800

    Add script for TGI serving

---
+++ scripts/serve_tgi.sh
@@ -0,0 +1,7 @@
+model=meta-llama/Meta-Llama-3-8B-Instruct
+volume=$HF_HOME # share a volume with the Docker container to avoid downloading weights every run
+port=8010
+
+docker run --gpus all --shm-size 64g -p $port:80 -v $volume:/data \
+    ghcr.io/huggingface/text-generation-inference \
+    --model-id $model
